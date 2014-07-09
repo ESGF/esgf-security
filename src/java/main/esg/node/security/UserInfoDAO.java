de590165cee063a7a5a8fa91165f266955200bae
kleanthis <kleanthis@mist.badc.rl.ac.uk>
2014-07-09 16:26:41 +0100

    Some comments changed.

--- src/java/main/esg/node/security/UserInfoDAO.java
+++ src/java/main/esg/node/security/UserInfoDAO.java
@@ -892,8 +892,8 @@ public class UserInfoDAO {
     }
    
     
-    /* kltsa 04/06/2014 change for issue 23061  : Search database based on the supplied username, password 
-     *                                            also corresponding return openid. 
+    /* kltsa 04/06/2014 change for issue 23061  : Searches database based on the supplied username, password. 
+     *                                            Also corresponding openid is returned. 
      */
     public boolean check_user_Password(String username, String queryPassword, StringBuilder openid) 
     {
